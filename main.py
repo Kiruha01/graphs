ea932c1a8a6281921e3326a5156817c26a6ebe13
Kiruha01 <liss-2001g@mail.ru>
2022-05-09 17:19:12 +0300

    Сильная связность

--- main.py
+++ main.py
@@ -17,18 +17,19 @@ google_graph = google_directed('.google.txt')
 # ca_graph = ca_undirected('.ca.txt')
 # vk_graph = vk_directed('.test.csv')
 
-v = len(google_graph.get_all_vertices())
-e = google_graph.num_edges
-print(f"v: {v}, e: {e}")
-print("part:", e/(2*(v**2-v)))
+total_v = len(google_graph.get_all_vertices())
+total_e = google_graph.num_edges
+print(f"v: {total_v}, e: {total_e}")
+print("part:", total_e/(2*(total_v**2-total_v)))
 
 # Слабая связность
+
 unvisited = set(google_graph.get_all_vertices())
-num_of_comps = 0
+comps = []
 
 while unvisited:
     start = unvisited.pop()
-    num_of_comps += 1
+    comps.append([])
     stack = [start]
     while stack:
         v = stack[-1]
@@ -44,9 +45,54 @@ while unvisited:
                     break
             else:
                 stack.pop(-1)
+                comps[-1].append(v)
 
-print("Сдабая св", num_of_comps)
+print("Сдабая св", comps)
+max_comp = max(comps, key=lambda x: len(x))
+print("Доля вершин:", len(max_comp)/total_v)
 
+# Сильная связность
+
+ver_order = []
+unvisited = set(google_graph.get_all_vertices())
+while unvisited:
+    # DFS on inverted graph
+    start = unvisited.pop()
+    stack = [start]
+    while stack:
+        v = stack[-1]
+        unvisited.discard(v)
+        for in_edge in google_graph.incoming_adj_list[v]:
+            if in_edge.start in unvisited:
+                stack.append(in_edge.start)
+                break
+        else:
+            stack.pop(-1)
+            ver_order.append(v)
+ver_order.reverse()
+
+# DFS on normal graph with new order
+unvisited = set(google_graph.get_all_vertices())
+comps = []
+while unvisited:
+    start = sorted(unvisited, key=lambda x: ver_order.index(x))[0]
+    stack = [start]
+    comps.append([])
+    while stack:
+        v = stack[-1]
+        unvisited.discard(v)
+        s = sorted(google_graph.outgoing_adj_list[v], key=lambda edge: ver_order.index(edge.end))
+        for out_edge in sorted(google_graph.outgoing_adj_list[v], key=lambda edge: ver_order.index(edge.end)):
+            if out_edge.end in unvisited:
+                stack.append(out_edge.end)
+                break
+        else:
+            stack.pop(-1)
+            comps[-1].append(v)
+
+print('Strong: ', comps)
+max_comp = max(comps, key=lambda x: len(x))
+print("Доля вершин:", len(max_comp)/total_v)
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
