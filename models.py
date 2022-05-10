7e18621847466e3bb10ab9837c93fa5facbaafa7
Kiruha01 <liss-2001g@mail.ru>
2022-05-10 20:21:28 +0300

    Add delete_vertex method

--- models.py
+++ models.py
@@ -44,6 +44,30 @@ class BaseGraph:
     def is_weighted(self):
         raise NotImplementedError()
 
+    def delete_vertex(self, v: int) -> None:
+        end_vertices = []
+        while self.outgoing_adj_list[v]:
+            end_vertices.append(self.outgoing_adj_list[v][0].end)
+            self.outgoing_adj_list[v].pop(0)
+
+        if not self.incoming_adj_list:
+            return
+
+        for end in end_vertices:
+            idx = [idx for idx, x in enumerate(self.incoming_adj_list[end]) if x.start == v][0]
+            self.incoming_adj_list[end].pop(idx)
+
+        start_vertices = []
+        while self.incoming_adj_list[v]:
+            start_vertices.append(self.incoming_adj_list[v][0].start)
+            self.incoming_adj_list[v].pop(0)
+
+        for start in start_vertices:
+            idx = [idx for idx, x in enumerate(self.outgoing_adj_list[start]) if x.end == v][0]
+            self.outgoing_adj_list[start].pop(idx)
+
+        print(end_vertices, start_vertices)
+
     @property
     def num_vertices(self):
         return len(self.get_all_vertices())
