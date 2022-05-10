9fa06eafabe14e344ca8f004d12f23da0286f75f
GrigorevGrigorii <nigddd@gmail.com>
2022-05-10 16:42:02 +0300

    Solve 1.2

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from import_dataset import google_directed, ca_undirected, vk_directed
-from utils import weak_conns, strong_conns
+from utils import weak_conns, strong_conns, evaluate_main_characteristics
 
 # =========== Google =============
 
@@ -7,55 +7,68 @@ print('\nGoogle!!\n\n')
 
 google_graph = google_directed('google.txt')
 
-total_v = len(google_graph.get_all_vertices())
+total_v = google_graph.num_vertices
 total_e = google_graph.num_edges
 print(f"v: {total_v}, e: {total_e}")
-print("part:", total_e/(2*(total_v**2-total_v)))
+print("part:", total_e / (2 * (total_v ** 2 - total_v)))
 
 comps = weak_conns(google_graph)
 print("Слабая связность", comps)
-max_comp = max(comps, key=lambda x: len(x))
-print("Доля вершин:", len(max_comp)/total_v)
+max_weak_comp = max(comps, key=len)
+print("Доля вершин:", len(max_weak_comp) / total_v)
 
 comps = strong_conns(google_graph)
-print('Strong: ', comps)
-max_comp = max(comps, key=lambda x: len(x))
-print("Доля вершин:", len(max_comp)/total_v)
+print('Сильная связность: ', comps)
+max_strong_comp = max(comps, key=len)
+print("Доля вершин:", len(max_strong_comp) / total_v)
+
+radius, diameter, percentile = evaluate_main_characteristics(google_graph, max_weak_comp)
+print("Радиус: ", radius)
+print("Диаметр: ", diameter)
+print("90 процентиль расстояния: ", percentile)
 
 # ============ CA =============
 
 print('\nCA!!\n\n')
 
 ca_graph = ca_undirected('ca.txt')
-total_v = len(ca_graph.get_all_vertices())
+total_v = ca_graph.num_vertices
 total_e = ca_graph.num_edges
 print(f"v: {total_v}, e: {total_e}")
-print("part:", total_e/(2*(total_v**2-total_v)))
+print("part:", total_e / (2 * (total_v ** 2 - total_v)))
 
 comps = weak_conns(ca_graph)
 print("Слабая связность", comps)
-max_comp = max(comps, key=lambda x: len(x))
-print("Доля вершин:", len(max_comp)/total_v)
+max_comp = max(comps, key=len)
+print("Доля вершин:", len(max_comp) / total_v)
+
+radius, diameter, percentile = evaluate_main_characteristics(ca_graph, [3, 5, 2, 1])
+print("Радиус: ", radius)
+print("Диаметр: ", diameter)
+print("90 процентиль расстояния: ", percentile)
 
 # ============== VK =================
 
 print('\nVK!!\n\n')
 
-
 vk_graph = vk_directed('vk.csv')
 
-total_v = len(vk_graph.get_all_vertices())
+total_v = vk_graph.num_vertices
 total_e = vk_graph.num_edges
 print(f"v: {total_v}, e: {total_e}")
-print("part:", total_e/(2*(total_v**2-total_v)))
+print("part:", total_e / (2 * (total_v ** 2 - total_v)))
 
 comps = weak_conns(vk_graph)
 print("Слабая связность", comps)
-max_comp = max(comps, key=lambda x: len(x))
-print("Доля вершин:", len(max_comp)/total_v)
+max_weak_comp = max(comps, key=len)
+print("Доля вершин:", len(max_weak_comp) / total_v)
 
 comps = strong_conns(vk_graph)
-print('Strong: ', comps)
-max_comp = max(comps, key=lambda x: len(x))
-print("Доля вершин:", len(max_comp)/total_v)
+print('Сильная связность: ', comps)
+max_strong_comp = max(comps, key=len)
+print("Доля вершин:", len(max_strong_comp) / total_v)
 
+radius, diameter, percentile = evaluate_main_characteristics(vk_graph, max_weak_comp)
+print("Радиус: ", radius)
+print("Диаметр: ", diameter)
+print("90 процентиль расстояния: ", percentile)
