4da43242e49b57fb07c1e94014eff6fe7c4acadc
Dmitry913 <lazer01@inbox.ru>
2022-05-25 21:28:46 +0300

    add LCA

--- landmarks.py
+++ landmarks.py
@@ -1,6 +1,7 @@
 from enum import Enum
-from typing import Callable, List, Optional
+from typing import Callable, List, Optional, Set, Dict
 from random import sample
+from collections import deque
 
 from models import BaseGraph
 from utils import get_shortest_path_lengths
@@ -54,7 +55,7 @@ class LandmarksBasic:
         self._distances = {}
         self._prepare(graph)
 
-    def _prepare(self, graph: BaseGraph):
+    def _prepare(self, graph: BaseGraph) -> None:
         for landmark in self._landmarks:
             self._distances[landmark] = get_shortest_path_lengths(
                 graph, landmark, graph.get_all_vertices()
@@ -69,3 +70,52 @@ class LandmarksBasic:
             )
 
         return upper_bounder
+
+
+class LandmarksLCA(LandmarksBasic):
+    def _prepare(self, graph: BaseGraph) -> None:
+        for landmark in self._landmarks:
+            parents = {}
+            stack = deque([landmark])
+            while stack:
+                node = stack.popleft()
+                for n in graph.neighbors[node]:
+                    if n not in parents.keys():
+                        parents[n] = node
+                        stack.append(n)
+
+            self._distances[landmark] = parents
+
+    def distance(self, start: int, end: int) -> int:
+        upper_bounder = float("inf")
+        for landmark in self._landmarks:
+            lca_path = self._calculate_distance_over_by_landmark(landmark, start, end)
+            upper_bounder = min(
+                upper_bounder, len(lca_path) - 1
+            )
+
+        return upper_bounder
+
+    def _calculate_distance_over_by_landmark(self, landmark: int, start: int, end: int) -> List[int]:
+        SPT = self._distances[landmark]
+        path_from_start_to_landmark = self.get_path_to_set(SPT, start, {landmark})
+        path_from_end_to_path_start_landmark = self.get_path_to_set(SPT, end, set(path_from_start_to_landmark))
+
+        LCA = path_from_end_to_path_start_landmark[-1]
+
+        path = []
+        for node in path_from_start_to_landmark:
+            if node != LCA:
+                path.append(node)
+
+        path.extend(reversed(path_from_end_to_path_start_landmark))
+        return path
+
+    @staticmethod
+    def get_path_to_set(SPT_tree: Dict[int, int], _from: int, _target: Set[int]) -> List[int]:
+        next_node = _from
+        path = [next_node]
+        while next_node not in _target:
+            next_node = SPT_tree[next_node]
+            path.append(next_node)
+        return path
